7dc41702a0fd54b57427e82d816cc3fc7fbd7b40
Gaston Moure <gaston.moure@gmail.com>
2024-11-20 20:07:46 +0100

    add functionality sen email

---
+++ Functionalities/contactForm.js
@@ -0,0 +1,16 @@
+function sendMail() {
+  let params = {
+    from_name: document.getElementById('from_name').value,
+    from_email: document.getElementById('from_email').value,
+    message: document.getElementById('message').value,
+  }
+
+  emailjs.send('service_t6oxaez', 'template_4t8g932', params).then(
+    function () {
+      alert('Consulta enviada correctamente')
+    },
+    function (error) {
+      alert('Error al enviar su email. Error: ' + error)
+    }
+  )
+}
