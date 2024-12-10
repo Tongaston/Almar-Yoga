917f0a5e59e2ecce22b7d5121e8af78f06578734
Gaston Moure <gaston.moure@gmail.com>
2024-12-10 17:18:07 +0100

    server for enviroments variables

---
+++ server.js
@@ -0,0 +1,25 @@
+// server.js
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 3000
+
+require('dotenv').config()
+app.use(cors())
+
+// Servir archivos estáticos desde la carpeta "Almar-Yoga"
+app.use(express.static('Almar-Yoga'))
+
+// Endpoint para enviar las claves al cliente
+app.get('/emailjs-config', (req, res) => {
+  res.json({
+    publicKey: process.env.PUBLIC_KEY,
+    serviceId: process.env.SERVICE_ID,
+    templateId: process.env.TEMPLATE_ID,
+  })
+})
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
