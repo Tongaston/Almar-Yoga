e16e008c5ab6e26ca5d0e568000e8c2a6e956dcc
Gaston Moure <gaston.moure@gmail.com>
2024-12-10 17:19:15 +0100

    add contactForm functionalitie

--- Functionalities/contactForm.js
+++ Functionalities/contactForm.js
@@ -1,16 +1,40 @@
-function sendMail() {
-  let params = {
-    from_name: document.getElementById('from_name').value,
-    from_email: document.getElementById('from_email').value,
-    message: document.getElementById('message').value,
-  }
+// script.js
+// Espera a que el DOM esté completamente cargado
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('contact-form')
 
-  emailjs.send(process.env.SERVICE_ID, process.env.TEMPLATE_ID, params).then(
-    function () {
-      alert('Consulta enviada correctamente')
-    },
-    function (error) {
-      alert('Error al enviar su email. Error: ' + error)
-    }
-  )
-}
+  // Obtener la configuración de EmailJS desde el backend
+  fetch('http://localhost:3000/emailjs-config')
+    .then((response) => response.json())
+    .then((config) => {
+      // Inicializar EmailJS con el userId
+      emailjs.init(config.publicKey)
+
+      // Escuchar el envío del formulario
+      form.addEventListener('submit', (event) => {
+        event.preventDefault() // Evitar recarga de la página
+
+        // Capturar datos del formulario
+        const formData = {
+          form_name: document.getElementById('form_name').value,
+          form_email: document.getElementById('form_email').value,
+          message: document.getElementById('message').value,
+        }
+
+        // Enviar el correo usando EmailJS
+        emailjs
+          .send(config.serviceId, config.templateId, formData)
+          .then((response) => {
+            alert('Correo enviado exitosamente!')
+            form.reset() // Limpiar el formulario
+          })
+          .catch((error) => {
+            console.error('Error al enviar el correo:', error)
+            alert('Hubo un problema al enviar el correo.')
+          })
+      })
+    })
+    .catch((error) => {
+      console.error('Error al obtener la configuración de EmailJS:', error)
+    })
+})
